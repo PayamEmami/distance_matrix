6dcb9aeffae4deeb2d9f3ff0bee4167ec26c670e
PayamEmami <payam.emami@scilifelab.se>
2020-10-09 12:10:25 +0200

    Update distance_matrix.R

--- distance_matrix.R
+++ distance_matrix.R
@@ -1,5 +1,6 @@
-dmatrix.dissimilarity.fast <-   function(x, nblocks = 5,
-           threads = 2, ffuse = F, SampleSize = 250, use = "pairwise.complete.obs",pathSave="partialCors/",fileuse=F,normal=T,HiClimR=F, ...){
+dmatrix.spearman.dissimilarity.fast <- 
+  function(x, nblocks = 5,
+           threads = 2, ffuse = F, SampleSize = 250, use = "pairwise.complete.obs",pathSave="partialCors/",fileuse=F,normal=T,HiClimR=F,big=F, ...){
     if(normal)
     {
       corMAT<-1- cor(x, use = use,...)
@@ -11,15 +12,20 @@ dmatrix.dissimilarity.fast <-   function(x, nblocks = 5,
     }
     
     library(tidyverse)
-    require(doFuture)
-    require(foreach)
+  
     require(ff)
     require(psych)
     require(Matrix)
     library(data.table)
     # initialize parallel framework
-    plan(tweak(multiprocess,workers=threads))
-    registerDoFuture()
+    if(!big)
+    {
+      require(doFuture)
+      require(foreach)
+      plan(tweak(multiprocess,workers=threads))
+      registerDoFuture()
+    }
+
     
     NCOL <- ncol(x)
     
@@ -30,8 +36,18 @@ dmatrix.dissimilarity.fast <-   function(x, nblocks = 5,
     corMAT<-NA
     if(!fileuse)
     {
-      if (!ffuse) {corMAT <- matrix(nrow = NCOL, ncol = NCOL)}
-      if (ffuse) {corMAT<-ff(vmode = "single", dim = c(NCOL, NCOL))}
+      if (!ffuse & !big) {corMAT <- matrix(nrow = NCOL, ncol = NCOL)}
+      if (ffuse) {corMAT<-ff(vmode = "single", dim = c(NCOL, NCOL),filename="/vagrant/file.ff")}
+      if(big){
+        library(bigmemory)
+        corMAT<-bigmemory::big.matrix(nrow = NCOL, ncol = NCOL, type = 'double',
+                                           # type = "double", 
+                                            separated = FALSE,
+                                            backingfile = "example2.bin", 
+                                            descriptorfile = "example2.desc",
+                                      dimnames = list(colnames(x),colnames(x)))
+        
+        }
     }else{
       
       if(!dir.exists(pathSave))
@@ -53,6 +69,9 @@ dmatrix.dissimilarity.fast <-   function(x, nblocks = 5,
     ## iterate through each block combination, calculate correlation matrix
     ## between blocks and store them in the preallocated matrix on both
     ## symmetric sides of the diagonal
+   if(!big)
+   {
+     
    
     results <- foreach(i = 1:nrow(COMBS),.packages = c("ff","psych","data.table","Matrix"),.export = c("corMAT")) %dopar% {
       COMB <- COMBS[i, ]
@@ -79,9 +98,41 @@ dmatrix.dissimilarity.fast <-   function(x, nblocks = 5,
       B2 <- NULL
     
     }
+   }else{
+     
+     library(foreach)
+     library(doSNOW)
+     cl <- makeSOCKcluster(names=rep("localhost", threads)) # make a cluster of 4 machines
+     registerDoSNOW(cl) # register machines for foreach()
+     #options(bigmemory.allow.dimnames=TRUE)
+     xdesc <- describe(corMAT) 
+     
+  
+     foreach(i = 1:nrow(COMBS)) %dopar% {
+       library("bigmemory") 
+       library(data.table)
+       corMAT_tmp<- attach.big.matrix(xdesc)  
+     COMB <- COMBS[i, ]
+     G1 <- SPLIT[[COMB[1]]]
+     G2 <- SPLIT[[COMB[2]]]
+    # print(i)
+     
+      COR <- 1-cor(x[, G1, with = F], x[, G2, with = F], use = use,...)
+      corMAT_tmp[G1, G2] <- COR
+      corMAT_tmp[G2, G1] <- t(COR)
+       
+     COR <- NULL
+     # COR <- NULL
+     B1 <- NULL
+     B2 <- NULL
+     
+     }
+     stopCluster(cl)
+     gc()
+     return(corMAT)
+   }
     # 
-    # stopCluster(Cluster)
-    # gc()
+ 
  
     if(!fileuse)
     {
